507bdea97a9f041ceda9ef3afcdc57f7958c2141
Zach Wegner <zwegner@gmail.com>
2019-11-15 12:25:30 -0600

    Add MIT license to library

--- z_validate.c
+++ z_validate.c
@@ -1,7 +1,29 @@
+// faster-utf8-validator
+//
+// Copyright (c) 2019 Zach Wegner
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <stdint.h>
 #include <immintrin.h>
 
-// Faster UTF-8 SIMD validator
+// How this validator works:
 //
 //   [[[ UTF-8 refresher: UTF-8 encodes text in sequences of "code points",
 //   each one from 1-4 bytes. For each code point that is longer than one byte,
